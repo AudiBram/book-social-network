28f1c24224971d4276c4c8c30fd40988ce41aa4e
Audi Bramastya <audibram14@gmail.com>
2024-07-19 11:21:55 +0700

    Implement security config and authentication provider

---
+++ book-network-api/src/main/java/org/project/booknetworkapi/config/BeansConfig.java
@@ -0,0 +1,31 @@
+package org.project.booknetworkapi.config;
+
+import lombok.RequiredArgsConstructor;
+import org.project.booknetworkapi.security.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class BeansConfig {
+
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsServiceImpl);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
